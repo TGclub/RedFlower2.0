88eaa2bf479b8abf9fbb7776ef745ac069f7494d
JinxinYang <704911010@qq.com>
2018-10-11 18:55:08 +0800

    rewrite /image

--- src/main/resources/import.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO user (definition,gender,name,openid,wxid) VALUES ("你好",0,"张三","25555s5c2f","f5h8d");
-INSERT INTO user (definition,gender,name,openid,wxid) VALUES ("hello",1,"李四","54dsv4dv5","5xcv8ds");
